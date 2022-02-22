8d81ab96be92e770f066d62db4c0bafc297da3d8
Mateo Rodriguez <mateoytomas2009@hotmail.com>
2022-02-22 18:09:33 -0300

    Creacion springboot y dependencias, clase employee morel y repository

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,86 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 25, nullable = false)
+    private String firstName;
+
+    @Column(length = 25, nullable = false)
+    private String lastname;
+
+    @Column(length = 10, nullable = false, unique = true)
+    private String employeeid;
+
+    //Constructores
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastname, String employeeid) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.employeeid = employeeid;
+    }
+
+    //Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(String employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return getId().equals(employee.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", employeeid='" + employeeid + '\'' +
+                '}';
+    }
+}
